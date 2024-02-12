9182ade7732863c3b845f0afe61974f36b29e44d
Kessiii <kesvi@icloud.com>
2024-02-12 17:05:58 -0500

    Playing Around
    Callbacks and voices

--- Experiements/p5js Speech Experiements/js/script.js
+++ Experiements/p5js Speech Experiements/js/script.js
@@ -8,7 +8,10 @@ author, and this description to match your project!
 
 "use strict";
 
-let voice = new p5.Speech();
+const speechSynthesizer = new p5.Speech();
+
+let showSubtitle = false;
+let toSay = 'Hello world!';
 
 /**
 Description of preload
@@ -22,17 +25,41 @@ function preload() {
 Description of setup
 */
 function setup() {
+    createCanvas(600,600)
+    console.log(speechSynthesizer.listVoices());
 
-}
+    //synthesis
+    speechSynthesizer.setPitch(0.2);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice('Google UK English Male');
+
+    speechSynthesizer.onStart = speechStarted;
+    speechSynthesizer.onEnd = speechEnded; 
+    
+}    
 
 
 /**
 Description of draw()
 */
 function draw() {
+    background(244, 125, 199)
 
+    if (showSubtitle) {
+        textSize(36);
+        text(toSay, 100, 100);
+    }
 }
 
 function mousePressed() {
-    voice.speak('hello federico, I have been watching you from the screens, you need to wake up.')
+    speechSynthesizer.speak(toSay);
+}
+
+
+function speechStarted() {
+    showSubtitle = true;
+}
+
+function speechEnded() {
+    showSubtitle = false
 }
