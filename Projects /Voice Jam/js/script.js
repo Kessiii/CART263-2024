ab6e40b1641508be137640f58ddc2f307afdbc92
Kessiii <kesvi@icloud.com>
2024-02-13 23:33:04 -0500

    Light
    Tried to make the background reactive to the voice and centered it to the canvas. Made it so it looks like the pattern is glowing.

--- Projects /Voice Jam/js/script.js
+++ Projects /Voice Jam/js/script.js
@@ -43,9 +43,8 @@ function draw() {
     let g = map(-sin(frameCounter), -1, 1, 100, 200)
     let b = map(cos(frameCounter), -1, 1, 200, 100)
 
-    // let locX = mouseX - height / 2;
-    // let locY = mouseY - width / 2;
-    // pointLight(r, g, b, locX, locY, -100);
+    lightFalloff(0.97, 0, 0);
+    pointLight(r, g, b, 0, 40, sin(frameCounter * 4) * 50 - 120);
 
     noFill()
     stroke(255)
@@ -72,13 +71,13 @@ function draw() {
 
         stroke(r, g, b)
 
+        let z = sin(frameCounter * 4 + i * 5) * 150
+
         beginShape()
         rotateZ(frameCounter/rad)
-
         for (let j = 0; j < 360; j += 60) {
             let x = rad * cos(j)
             let y = rad * sin(j)
-            let z = sin(frameCounter * 4 + i * 5) * 150
 
             vertex(x,y,z)
         }
