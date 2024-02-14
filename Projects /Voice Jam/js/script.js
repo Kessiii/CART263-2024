70abc528aa6af989ea49f9c7514d91d7bd617b66
Kessiii <kesvi@icloud.com>
2024-02-13 23:00:09 -0500

    Combining
    Finally added the speech functions to the code. Now playing aroud. with the background to react with my voice

--- Projects /Voice Jam/js/script.js
+++ Projects /Voice Jam/js/script.js
@@ -7,6 +7,13 @@ I wanted to create a generative animation/art that moves in
 
 "use strict";
 
+const rec = new p5.SpeechRec();
+rec.continuous = true;
+rec.interimResults = true;
+rec.onResult = playWave; 
+rec.start();
+let start = 0;
+let frameCounter = 0
 
 /**
 Description of preload
@@ -32,31 +39,53 @@ Description of draw()
 function draw() {
     background(100, 0, 0)
 
-    rotateX(15)
+    let r = map(sin(frameCounter / 2), -1, 1, 100, 200)
+    let g = map(-sin(frameCounter), -1, 1, 100, 200)
+    let b = map(cos(frameCounter), -1, 1, 200, 100)
+
+    // let locX = mouseX - height / 2;
+    // let locY = mouseY - width / 2;
+    // pointLight(r, g, b, locX, locY, -100);
 
     noFill()
     stroke(255)
+    rotateX(15)
 
-    for (var i = 0; i < 90; i++) {
+    push()
+    translate(0,0, -200)
+    noStroke()
+    ambientMaterial(250);
+    plane(2000, 2000)
+    pop()
 
-        var r = map(sin(frameCount / 2), -1, 1, 100, 200)
-        var g = map(i, 0, 50, 100, 200)
-        var b = map(cos(frameCount), -1, 1, 200, 100)
+
+    if (Date.now() - 500 < start){
+        frameCounter++;
+    }
+
+    for (let i = 1; i <= 30; i++) {
+        let rad = i * 8
+
+        let r = map(sin(frameCounter / 2), -1, 1, 100, 200)
+        let g = map(i, 0, 50, 100, 200)
+        let b = map(cos(frameCounter), -1, 1, 200, 100)
 
         stroke(r, g, b)
 
-        rotate(0)
-
-
         beginShape()
-        for (var j = 0; j < 360; j += 60) {
-            var rad = i * 8
-            var x = rad * cos(j)
-            var y = rad * sin(j)
-            var z = sin(frameCount * 4 + i * 5) * 150
+        rotateZ(frameCounter/rad)
+
+        for (let j = 0; j < 360; j += 60) {
+            let x = rad * cos(j)
+            let y = rad * sin(j)
+            let z = sin(frameCounter * 4 + i * 5) * 150
 
             vertex(x,y,z)
         }
         endShape(CLOSE)
     }
+
+}
+function playWave() {
+    start = Date.now();
 }
