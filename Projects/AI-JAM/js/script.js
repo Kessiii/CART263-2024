1a98c55db3117fa29a71456315c34b612bae30d0
Kessiii <kesvi@icloud.com>
2024-02-27 19:06:41 -0500

    Added perlin rectangles with hand pose

--- Projects/AI-JAM/js/script.js
+++ Projects/AI-JAM/js/script.js
@@ -1,13 +1,9 @@
 /**
 Enveloppe Yourself 
 Kestrel Villapando
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
 */
 
 "use strict";
-
 
 /**
 Description of preload
@@ -15,17 +11,30 @@ Description of preload
 let handpose;
 let video;
 let predictions = [];
+const [baseX, baseY] = [640, 480];
+let scaleFactor = 1.6;
+let inc = 0.1;
+let zoff = 0;
+
+const maxRectSize = 8;
+const spacing = 0.2;
+
+const keypointRadius = 25;
 
 function setup() {
-  createCanvas(640, 480);
+  noiseDetail(8, 0.65);
   video = createCapture(VIDEO);
+
+  scaleFactor = windowHeight / baseY;
+  createCanvas(baseX * scaleFactor, baseY * scaleFactor);
+
   video.size(width, height);
 
   handpose = ml5.handpose(video, modelReady);
 
   // This sets up an event that fills the global variable "predictions"
   // with an array every time new hand poses are detected
-  handpose.on("predict", results => {
+  handpose.on("predict", (results) => {
     predictions = results;
   });
 
@@ -38,21 +47,91 @@ function modelReady() {
 }
 
 function draw() {
-  image(video, 0, 0, width, height);
+  // console.log(width, height);
+  translate(width, 0);
+  scale(-1, 1);
 
+  background(255);
   // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
+  drawPerlin();
+  // drawKeypoints();
+}
+
+function drawPerlin() {
+  let xoff = 0; // Start xoff at 0
+
+  // Loop over horizontal pixels
+  for (let x = 0; x < width; x += maxRectSize + spacing) {
+    let yoff = 0; // For every x, start yoff at 0
+    // Loop over vertical pixels
+    for (let y = 0; y < height; y += maxRectSize + spacing) {
+      let nearKeypoint = true;
+      let distScale = 0;
+
+      for (const prediction of predictions) {
+        for (const keypoint of prediction.landmarks) {
+          const kpx = keypoint[0] * scaleFactor;
+          const kpy = keypoint[1] * scaleFactor;
+          const kpz = keypoint[2] * scaleFactor;
+          const dist = Math.sqrt((x - kpx) ** 2 + (y - kpy) ** 2);
+          if (dist < keypointRadius) {
+            console.log(kpz);
+            distScale = Math.abs(dist - keypointRadius) / keypointRadius;
+
+            nearKeypoint = false;
+            break;
+          }
+        }
+        if (nearKeypoint) break;
+      }
+
+      yoff += inc; // Increment yoff
+
+      let noiseVal = noise(xoff, yoff, zoff);
+      push();
+      translate(x, y);
+      // rotate(TWO_PI * noiseVal);
+      noStroke();
+      fill(0);
+      if (!nearKeypoint) {
+        noiseVal += 0.4 * (distScale + 0.1);
+      }
+      rect(
+        0,
+        0,
+        (noiseVal - 0.5) * maxRectSize,
+        (noiseVal - 0.5) * maxRectSize
+      );
+      pop();
+    }
+    xoff += inc; // Increment xoff
+  }
+  zoff += 0.01; // Increment zoff
 }
 
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
-  for (let i = 0; i < predictions.length; i += 1) {
-    const prediction = predictions[i];
-    for (let j = 0; j < prediction.landmarks.length; j += 1) {
-      const keypoint = prediction.landmarks[j];
+  for (const prediction of predictions) {
+    for (const section of Object.values(prediction.annotations)) {
+      for (let i = 0; i < section.length - 1; i++) {
+        const [x, y, z] = section[i];
+        const [nextX, nextY, nextZ] = section[i + 1];
+        fill(0, 255, 0);
+        noStroke();
+        ellipse(x * scaleFactor, y * scaleFactor, 10, 10);
+        stroke(0, 255, 0);
+        line(
+          x * scaleFactor,
+          y * scaleFactor,
+          nextX * scaleFactor,
+          nextY * scaleFactor
+        );
+      }
+
+      const [x, y, z] = section[section.length - 1];
       fill(0, 255, 0);
       noStroke();
-      ellipse(keypoint[0], keypoint[1], 10, 10);
+      ellipse(x * scaleFactor, y * scaleFactor, 10, 10);
     }
   }
 }
