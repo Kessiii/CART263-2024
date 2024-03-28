f3a9e8979a4f77e588557f7c31882ecd810a829b
Kessiii <kesvi@icloud.com>
2024-03-28 01:27:21 -0400

    Comments
    Organized myself

--- Projects/Game-Engine-Jam/js/Game.js
+++ Projects/Game-Engine-Jam/js/Game.js
@@ -1,8 +1,24 @@
+//This is the Phaser3 config file
+
+//Game Scene
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+
+  //Setting background color
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // Placing it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
 
 const game = new Phaser.Game(config);
