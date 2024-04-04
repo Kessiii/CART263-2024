bee0e576a2455b5d19166e7a58bd2f89ca11d312
Kessiii <kesvi@icloud.com>
2024-04-04 00:54:00 -0400

    Pew
    the shape is shooting some pews

--- Projects/Game-Engine-Jam/js/playScene.js
+++ Projects/Game-Engine-Jam/js/playScene.js
@@ -4,6 +4,7 @@ class PlayScene extends Phaser.Scene {
 
     this.background = null;
     this.shape = null;
+    this.firePew = false;
   }
 
   init(data) {
@@ -15,6 +16,7 @@ class PlayScene extends Phaser.Scene {
 
     this.load.image("lineBackground", "./assets/images/play_Screen.png");
     this.load.image("shape", "./assets/images/shape.png");
+    this.load.image("pew", "./assets/images/pewpew.png");
   }
 
   create(data) {
@@ -24,23 +26,43 @@ class PlayScene extends Phaser.Scene {
     this.shape = this.physics.add
       .sprite(1920 / 2, 1080 - 100, "shape")
       .setScale(0.2);
+
+    //Pewpew group
+    this.pewGroup = this.physics.add.group();
   }
 
   update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey("LEFT");
     const keyRightObj = this.input.keyboard.addKey("RIGHT");
+    const keySpaceObj = this.input.keyboard.addKey("SPACE");
 
     if (keyLeftObj.isDown === true) {
-      this.shape.x = this.shape.x -= 20;
+      this.shape.x = this.shape.x -= 15;
       if (this.shape.x < 0) {
         this.shape.x = 0;
       }
     }
     if (keyRightObj.isDown === true) {
-      this.shape.x = this.shape.x += 20;
+      this.shape.x = this.shape.x += 15;
       if (this.shape.x > 1920) {
         this.shape.x = 1920;
       }
+    }
+
+    if (keySpaceObj.isDown == true) {
+      if (this.firePew === false) {
+        this.firePew = true;
+        const aNewPew = this.physics.add.sprite(
+          this.shape.x,
+          this.shape.y,
+          "pew"
+        );
+        this.pewGroup.add(aNewPew);
+      }
+    }
+
+    if (keySpaceObj.isUp == true) {
+      this.firePew = false;
     }
   }
 }
