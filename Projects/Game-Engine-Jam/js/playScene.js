320c67369b4e76ee88049ef84fa1ef38bc6d53bc
Kessiii <kesvi@icloud.com>
2024-04-04 00:30:16 -0400

    Shape!
    I guess since I am turning this game into a shooter instead, this would be the ship.

--- Projects/Game-Engine-Jam/js/playScene.js
+++ Projects/Game-Engine-Jam/js/playScene.js
@@ -1,6 +1,9 @@
 class PlayScene extends Phaser.Scene {
   constructor() {
     super({ key: "playScene" });
+
+    this.background = null;
+    this.shape = null;
   }
 
   init(data) {
@@ -9,9 +12,19 @@ class PlayScene extends Phaser.Scene {
 
   preload() {
     console.log("Play Scene");
+
+    this.load.image("lineBackground", "./assets/images/play_Screen.png");
+    this.load.image("shape", "./assets/images/shape.png");
   }
 
-  create(data) {}
+  create(data) {
+    this.background = this.add.image(0, 0, "lineBackground").setScale(1.5);
+    this.background.setOrigin(0, 0);
+
+    this.shape = this.physics.add
+      .sprite(1920 / 2, 1080 - 100, "shape")
+      .setScale(0.2);
+  }
 
   update(time, delta) {}
 }
